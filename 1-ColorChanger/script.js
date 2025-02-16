59c928f5f8e5d69dcf60ab65ee2f8070f86890a4
Alshifa Fatima <fatimaalshifa38@gmail.com>
2025-02-16 16:13:59 +0000

    colorChanger

---
+++ 1-ColorChanger/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function(button){
+    console.log(butoon);
+    button.addEventListener('click' , function(event){
+        console.log(event);
+        console.log(event.target);
+
+        if (event.target.id === 'red') {
+            body.style.backgroundColor = event.target.id;
+        }
+        if (event.target.id === 'blue') {
+            body.style.backgroundColor = event.target.id;
+        }
+        if (event.target.id === 'orange') {
+            body.style.backgroundColor = event.target.id;
+        }
+        if (event.target.id === 'green') {
+            body.style.backgroundColor = event.target.id;
+        }
+    })
+})
